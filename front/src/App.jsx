1759a57b5b974e0fca48376f8256f544e9662741
stephanieEt <stetourneau@laposte.net>
2024-05-29 11:29:37 +0200

    create header and footer

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,16 @@
-import Header from "/src/components/Header";
+import Footer from "./components/Footer/Footer";
+import Header from "/src/components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes></Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- front/src/components/Header.jsx
+++
@@ -1,4 +0,0 @@
-const Header = () => {
-  return <h1>coucou</h1>;
-};
-export default Header;
