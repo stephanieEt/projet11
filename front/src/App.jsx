7b6057afb9ff9606dcf1b34a1d3ac0e3ff697be2
stephanieEt <stetourneau@laposte.net>
2024-05-29 17:52:02 +0200

    create route / and user

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,12 +2,17 @@ import Footer from "./components/Footer/Footer";
 import Header from "/src/components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
+import Accueil from "./components/page/Accueil";
+import Sign from "./components/page/Sign";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/user" element={<Sign />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ front/src/components/page/Sign.jsx
@@ -0,0 +1,28 @@
+const Sign = () => {
+  return (
+    <div className="body-sign-in">
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+          </form>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Sign;
