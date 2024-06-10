58dbb443b297e06a9b4f4d3b344545ca0a8ccb7c
stephanieEt <stetourneau@laposte.net>
2024-06-10 10:16:18 +0200

    create page error

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import User from "./pages/UserAccount";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from "./pages/Error";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/User" element={<User />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </BrowserRouter>
